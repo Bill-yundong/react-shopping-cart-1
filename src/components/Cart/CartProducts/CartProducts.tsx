160c080c9eb1ef361f46bf9ac070521b306df47a
Bill Wu <yundongsoftwareit@gmail.com>
2026-04-15 16:55:58 -0400

    bugfix购物车相关的bug

--- src/components/Cart/CartProducts/CartProducts.tsx
+++ src/components/Cart/CartProducts/CartProducts.tsx
@@ -11,9 +11,9 @@ const CartProducts = ({ products }: IProps) => {
   return (
     <S.Container>
       {products?.length ? (
-        <S.CartProductsEmpty>
-          Unable to load cart items
-        </S.CartProductsEmpty>
+        products.map((product) => (
+          <CartProduct key={product.sku} product={product} />
+        ))
       ) : (
         <S.CartProductsEmpty>
           Add some products in the cart <br />
